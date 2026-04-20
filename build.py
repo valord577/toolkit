27609743863d1607fe6c32c6af810e3bfd6bf426
valord577 <valord577@gmail.com>
2026-04-20 22:51:43 +0800

    refactor: modular programming in python3

--- build.py
+++ build.py
@@ -2,226 +2,151 @@
 
 # fmt: off
 
-import datetime as dt
-import importlib.util
-import os
-import platform
-import shutil
-import subprocess as sp
 import sys
+sys.dont_write_bytecode = True
+
+from scripts import utils as x
+# ----------------------------
+
+
+import datetime as dt
+import os
+import shutil
+
+from pathlib import Path
 from typing import NoReturn, Union
 
+if sys.version_info < (3, 6):
+    raise RuntimeError(f'Required Python Interpreter ≥ 3.6')
 
-PROJ_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
 # ----------------------------
 # optimize
 #  - 0 DEBUG
 #  - 1 RELEASE (default)
 # ----------------------------
-LIB_RELEASE = os.getenv('LIB_RELEASE', '1')
+LIB_RELEASE = os.getenv('LIB_RELEASE') or '1'
 if LIB_RELEASE != '0': LIB_RELEASE = '1'
 # ----------------------------
-# ci runtime
-# ----------------------------
-ON_GITLAB_CI = os.getenv('GITLAB_CI', '')      == 'true'
-ON_GITHUB_CI = os.getenv('GITHUB_ACTIONS', '') == 'true'
+
 
 class _ctx:
     def __init__(self):
-        self.script = self._lazy_import()
         self.gocmd_exec  = shutil.which('go') or 'go'
         self.extra_args  = []
-
-        self.native_plat = platform.system().lower()
-        self.native_arch = platform.machine().lower()
-        if self.native_arch == 'x86_64':  self.native_arch = 'amd64'
-        if self.native_arch == 'aarch64': self.native_arch = 'arm64'
+        self.goenv_buld  = {}
+        self.go_ldflags  = '-v'
+        self.optimization = 'default'
+        if LIB_RELEASE == '1':
+            self.go_ldflags += ' -s -w'
+            self.optimization = 'release'
+        if LIB_RELEASE == '0':
+            self.extra_args.extend(['-gcflags', '-N -l'])
+            self.optimization = 'debug'
 
         self.target_plat = ''
         self.target_arch = ''
         self.target_libc = ''
-        self.env_passthrough = {
-            'BUILD_ENV': {},
-        }
 
-        if not ON_GITHUB_CI:
-            self.env_passthrough['BUILD_ENV'].update({
+        if not x.ON_GITHUB_CI:
+            self.goenv_buld.update({
                 'GO111MODULE': 'on',
                 'GOSUMDB': 'sum.golang.google.cn',
                 'GOPROXY': 'https://goproxy.cn,direct',
             })
 
-    def _lazy_import(self):
-        name = 'build_steps.py'
-        path = os.path.abspath(os.path.join(PROJ_ROOT, name))
-        spec = importlib.util.spec_from_file_location('', path)
-        if not spec:
-            raise ModuleNotFoundError(f'missing `{name}`: "failed @importlib.util.spec_from_file_location"')
-        module = importlib.util.module_from_spec(spec)
-        try:
-            spec.loader.exec_module(module)  # type: ignore
-        except FileNotFoundError:
-            raise ModuleNotFoundError(f'missing `{name}`: "no such file [{path}]"')
-        if not hasattr(module, 'module_init'):
-            raise ModuleNotFoundError(f'missing `{name}`: "no attr [module_init]"')
-        return module
-
     def getenv(self) -> dict:
-        env = {
-            **self.env_passthrough,
+        _target_archlibc = self.target_arch
+        if self.target_libc:
+            _target_archlibc = f'{self.target_arch}-{self.target_libc}'
+        _pkg_inst_dir = (Path(x.PROJ_ROOT) / 'out' / self.target_plat / _target_archlibc).absolute().as_posix()
+        if (x.ON_GITLAB_CI or x.ON_GITHUB_CI) and (_pkg_inst_dir_ci := os.getenv('INST_DIR')):
+            _pkg_inst_dir = Path(_pkg_inst_dir_ci).absolute().as_posix()
+        if x.ON_CODE_EDIT:
+            _pkg_inst_dir = (Path(x.PROJ_ROOT) / 'out').absolute().as_posix()
+
+        return {
             **{
-                'PROJ_ROOT': PROJ_ROOT,
 
-                'LIB_RELEASE': LIB_RELEASE,
-
-                'FUNC_SHELL_DEVNUL': _util_func__subprocess_devnul,
-                'FUNC_SHELL_STDOUT': _util_func__subprocess_stdout,
+                'GOCMD_EXEC': self.gocmd_exec,
+                'EXTRA_ARGS': self.extra_args,
+                'GOENV_BULD': self.goenv_buld,
+                'GO_LDFLAGS': self.go_ldflags,
+                'OPTIMIZATION': self.optimization,
 
                 'PKG_PLATFORM': self.target_plat,
                 'PKG_ARCH': self.target_arch,
                 'PKG_LIBC': self.target_libc,
                 'PKG_ARCH_LIBC': self.target_arch,
 
-                'GOCMD_EXEC': self.gocmd_exec,
-                'EXTRA_ARGS': self.extra_args,
+                'PKG_INST_DIR': _pkg_inst_dir,
             },
         }
-        if env['PKG_LIBC']:
-            env['PKG_ARCH_LIBC'] = f"{env['PKG_ARCH']}-{env['PKG_LIBC']}"
-        env['PKG_INST_DIR'] = os.path.abspath(os.path.join(PROJ_ROOT, 'out', env['PKG_PLATFORM'], env['PKG_ARCH_LIBC']))
-        if ON_GITLAB_CI or ON_GITHUB_CI:
-            env['PKG_INST_DIR'] = os.getenv('INST_DIR') or env['PKG_INST_DIR']
-
-        return env
-
-
-def _self_func__tree(basepath: str, depth: int = 0):
-    # name, path, depth, is_last, is_symlink, is_dir
-    _stack = [(basepath, basepath, -1, '-1', os.path.islink(basepath), os.path.isdir(basepath))]
-    while _stack:
-        _name, _path, _depth, _is_last, _is_symlink, _is_dir = _entry = _stack.pop()
-        if _depth == -1:
-            print(_name, file=sys.stderr)
-        else:
-            print(f"{'│   ' * _depth}{'└── ' if _is_last == '1' else '├── '}{_name}{f' -> {os.readlink(_path)}' if _is_symlink else ''}", file=sys.stderr)
-
-        if (not _is_dir) or (depth > 0 and _depth + 1 >= depth):
-            continue
-        with os.scandir(_path) as it:
-            entries = sorted(it, key=lambda e: e.name)
-            entries_dir_first = [ d for d in entries if d.is_dir() ]
-            entries_dir_first.extend([ f for f in entries if not f.is_dir() ])
-            for i, entry in enumerate(reversed(entries_dir_first)):
-                _stack.append(
-                    (
-                        entry.name,
-                        entry.path,
-                        _depth + 1,
-                        '1' if i == 0 else '0',
-                        entry.is_symlink(),
-                        entry.is_dir(follow_symlinks=False),
-                    )
-                )
-def _util_func__subprocess_stdout(args: Union[str, list[str]],
-    cwd: Union[str, None] = None, env: Union[dict[str, str], None] = None, shell=False
-) -> str:
-    print(f'>>>> subprocess cmdline: {args}', file=sys.stderr)
-    proc = sp.run(args=args, cwd=cwd, env=env, shell=shell, stdout=sp.PIPE, text=True)
-    if proc.returncode != 0:
-        print(f'>>>> subprocess exitcode: {proc.returncode}', file=sys.stderr)
-        sys.exit(proc.returncode)
-    return proc.stdout
-def _util_func__subprocess_devnul(args: Union[str, list[str]],
-    cwd: Union[str, None] = None, env: Union[dict[str, str], None] = None, shell=False
-):
-    print(f'>>>> subprocess cmdline: {args}', file=sys.stderr)
-    proc = sp.run(args=args, cwd=cwd, env=env, shell=shell)
-    if proc.returncode != 0:
-        print(f'>>>> subprocess exitcode: {proc.returncode}', file=sys.stderr)
-        sys.exit(proc.returncode)
-
-
 
 
 def _setctx_linux(
     ctx: _ctx, _native: bool, _tuple: tuple[str, ...],
 ):
-    if ctx.native_plat != 'linux':
-        raise NotImplementedError(f'unsupported host os: {ctx.native_plat}')
-    ctx.env_passthrough['PLATFORM_LINUX'] = True
-
     if _native:
-        ctx.target_arch = ctx.native_arch
+        ctx.target_arch = x.NATIVE_ARCH
         if not (ctx.target_arch in ['arm64', 'amd64']):
-            raise NotImplementedError(f'unsupported target arch: {ctx.native_arch}')
+            raise NotImplementedError(f'unsupported target arch: {ctx.target_arch}')
     else:
-        CROSS_TOOLCHAIN_ROOT = os.getenv('CROSS_TOOLCHAIN_ROOT')
-        if not CROSS_TOOLCHAIN_ROOT:
-            raise GeneratorExit('missing required env: `CROSS_TOOLCHAIN_ROOT`')
+        CROSS_TOOLCHAIN_ROOT = x._util_get_cross_toolchain_dir()
 
         ctx.target_arch = _tuple[2]
         ctx.target_libc = _tuple[3]
-
-        _target_triple = ''
-        if ctx.target_arch == 'arm64':
-            _target_triple = f'aarch64-unknown-linux-{ctx.target_libc}'
-        if ctx.target_arch == 'amd64':
-            _target_triple = f'x86_64-pc-linux-{ctx.target_libc}'
-        if ctx.target_arch == 'armv7':
-            _target_triple = f'arm-unknown-linux-{ctx.target_libc}'
+        _target_triple = {
+            'arm64': f'aarch64-unknown-linux-{ctx.target_libc}',
+            'amd64': f'x86_64-pc-linux-{ctx.target_libc}',
+            'armv7': f'arm-unknown-linux-{ctx.target_libc}',
+        }[ctx.target_arch]
 
         # cgotool bin
         CROSS_TOOLCHAIN_CGOTOOL_PREFIX = os.getenv('CROSS_TOOLCHAIN_CGOTOOL_PREFIX')
         if not CROSS_TOOLCHAIN_CGOTOOL_PREFIX:
-            CROSS_TOOLCHAIN_CGOTOOL_PREFIX = os.path.abspath(os.path.join(CROSS_TOOLCHAIN_ROOT, 'cgotool-wrapper'))
+            CROSS_TOOLCHAIN_CGOTOOL_PREFIX = (Path(CROSS_TOOLCHAIN_ROOT) / 'cgotool-wrapper').absolute().as_posix()
         ctx.gocmd_exec = f'{CROSS_TOOLCHAIN_CGOTOOL_PREFIX}.{_target_triple}'
 def _setctx_apple(
     ctx: _ctx, _native: bool, _tuple: tuple[str, ...],
 ):
-    if ctx.native_plat != 'darwin':
-        raise NotImplementedError(f'unsupported host os: {ctx.native_plat}')
-    ctx.env_passthrough['PLATFORM_APPLE'] = True
-
     if _native:
-        ctx.target_arch = ctx.native_arch
+        ctx.target_arch = x.NATIVE_PLAT
         if not (ctx.target_arch in ['arm64', 'amd64']):
-            raise NotImplementedError(f'unsupported target arch: {ctx.native_arch}')
+            raise NotImplementedError(f'unsupported target arch: {ctx.target_arch}')
     else:
         ctx.target_arch = _tuple[1]
 
-        crossfiles_dir = os.path.abspath(os.path.join(PROJ_ROOT, '.crossfiles', 'apple'))
+        crossfiles_dir = (Path(x.PROJ_ROOT) / 'crossfiles' / 'apple')
         # cgotool bin
-        ctx.gocmd_exec = os.path.abspath(os.path.join(crossfiles_dir, f'cgotool-wrapper.{ctx.target_arch}'))
+        ctx.gocmd_exec = (Path(crossfiles_dir) / f'cgotool-wrapper.{ctx.target_arch}').absolute().as_posix()
 def _setctx_win32_mingw(
     ctx: _ctx, _native: bool, _tuple: tuple[str, ...],
 ):
-    if ctx.native_plat != 'linux':
-        raise NotImplementedError(f'unsupported host os: {ctx.native_plat}')
-    ctx.env_passthrough['PLATFORM_WIN32'] = True
-    ctx.env_passthrough['BUILD_ENV'].update({
+    ctx.goenv_buld.update({
         'GOOS': 'windows',
     })
 
-    CROSS_TOOLCHAIN_ROOT = os.getenv('CROSS_TOOLCHAIN_ROOT')
-    if not CROSS_TOOLCHAIN_ROOT:
-        raise GeneratorExit('missing required env: `CROSS_TOOLCHAIN_ROOT`')
+    CROSS_TOOLCHAIN_ROOT = x._util_get_cross_toolchain_dir()
 
     ctx.target_arch = _tuple[1]
-
-    _target_arch = ''
-    if ctx.target_arch == 'amd64': _target_arch = 'x86_64'
-    if ctx.target_arch == 'arm64': _target_arch = 'aarch64'
+    _target_arch = {
+        'arm64': f'aarch64',
+        'amd64': f'x86_64',
+    }[ctx.target_arch]
 
     # cgotool bin
     CROSS_TOOLCHAIN_CGOTOOL_PREFIX = os.getenv('CROSS_TOOLCHAIN_CGOTOOL_PREFIX')
     if not CROSS_TOOLCHAIN_CGOTOOL_PREFIX:
-        CROSS_TOOLCHAIN_CGOTOOL_PREFIX = os.path.abspath(os.path.join(CROSS_TOOLCHAIN_ROOT, 'cgotool-wrapper'))
+        CROSS_TOOLCHAIN_CGOTOOL_PREFIX = (Path(CROSS_TOOLCHAIN_ROOT) / 'cgotool-wrapper').absolute().as_posix()
     ctx.gocmd_exec = f'{CROSS_TOOLCHAIN_CGOTOOL_PREFIX}.{_target_arch}'
 
 
 _targets = {
     'linux': {
         'native': True,
+        'hostos': ('linux', ),
         'setctx': _setctx_linux,
         'tuples': [
             ('linux', 'crossbuild', 'amd64', 'gnu'),
@@ -234,6 +159,7 @@ _targets = {
     },
     'darwin': {
         'native': True,
+        'hostos': ('darwin', ),
         'setctx': _setctx_apple,
         'tuples': [
             ('darwin', 'arm64'),
@@ -242,6 +168,7 @@ _targets = {
     },
     'windows': {
         'native': False,
+        'hostos': ('linux', ),
         'setctx': _setctx_win32_mingw,
         'tuples': [
             ('windows', 'arm64'),
@@ -266,15 +193,11 @@ def show_help(exitcode = 1) -> NoReturn:
     help_str  = f'Usage: {sys.argv[0]} -h|--help\n'
     help_str += f'Usage: {sys.argv[0]} [target]\n\n'
     help_str += f'Target Options:\n{_targets_help_str}\n'
-    print(help_str[:-1], file=sys.stderr)
+    x.print_stderr(help_str[:-1])
     sys.exit(exitcode)
 
 
 if __name__ == "__main__":
-    if sys.version_info < (3, 6):
-        raise GeneratorExit(f'Required Python Interpreter ≥ 3.6')
-
-
     argv_tgt: list[str] = []
     argv = sys.argv[1:]; argc = len(argv); i = 0
     while i < argc:
@@ -283,19 +206,31 @@ if __name__ == "__main__":
             show_help(0)  # exited
         else:
             argv_tgt.append(arg)
-    argc_tgt = len(argv_tgt)
 
+    argc_tgt = len(argv_tgt)
+    if argc_tgt < 1:
+        if x.NATIVE_PLAT in ['linux', 'darwin']:
+            argc_tgt +=1; argv_tgt.append(x.NATIVE_PLAT)
 
     ctx = _ctx()
-    if argc_tgt < 1:
-        if not (ctx.native_plat in ['linux', 'darwin']):
-            raise NotImplementedError(f'unsupported native platform: {ctx.native_plat}')
-        argc_tgt +=1; argv_tgt.append(ctx.native_plat)
-
     ctx.target_plat = argv_tgt[0]
     _target = _targets.get(ctx.target_plat)
     if not _target:
         raise NotImplementedError(f'unsupported target platform: {ctx.target_plat}')
+    _hostos = _target['hostos']
+    if not isinstance(_hostos, tuple) \
+        or \
+        (len(_hostos) not in [1, 2]) \
+        or \
+        ((len(_hostos) == 1) and (
+            _hostos[0] != x.NATIVE_PLAT
+        )) \
+        or \
+        ((len(_hostos) == 2) and (
+            _hostos[0] != x.NATIVE_PLAT or _hostos[1] != x.NATIVE_ARCH
+        )):
+        raise NotImplementedError(f'unsupported host os: {_hostos}')
+
 
     _tuple: Union[tuple[str, ...], None] = None
     if argc_tgt > 1:
@@ -310,8 +245,13 @@ if __name__ == "__main__":
 
 
     build_env = ctx.getenv()
-    build_steps = ctx.script.module_init(build_env)
+    _pkg_inst_dir = build_env['PKG_INST_DIR']
+
+    build_steps = x._util_load_module(f'build_steps', ['module_init']).module_init(build_env)
     for func in build_steps:
         func()
-    _self_func__tree(build_env['PKG_INST_DIR'], depth=3)
-    print(f'──── Build Done @{dt.datetime.now(dt.timezone.utc).strftime("%Y-%m-%d %H:%M:%S %Z")} ────', file=sys.stderr)
+    if not x.ON_CODE_EDIT:
+        x._util_func__exec_python([
+            (Path(x.PROJ_ROOT) / 'scripts' / 'tree.py').absolute().as_posix(), _pkg_inst_dir, '2'
+        ])
+    x.print_stderr(f'──── Build Done @{dt.datetime.now(dt.timezone.utc).strftime("%Y-%m-%d %H:%M:%S %Z")} ────')
---
+++ scripts/utils.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+# fmt: off
+
+import importlib.util
+import io
+import json
+import subprocess as sp
+import os
+import platform
+import sys
+
+from pathlib import Path
+from typing import Union
+
+
+PROJ_ROOT = (Path(__file__).absolute().resolve().parents[1]).as_posix()
+# ----------------------------
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', line_buffering=True)
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', line_buffering=True)
+# ----------------------------
+# ci runtime
+# ----------------------------
+ON_GITLAB_CI = os.getenv('GITLAB_CI') or ''      == 'true'
+ON_GITHUB_CI = os.getenv('GITHUB_ACTIONS') or '' == 'true'
+ON_CODE_EDIT = (not ON_GITLAB_CI) and (not ON_GITHUB_CI) \
+    and (os.getenv('CLANGD_CODE_COMPLETION') or '0' == '1')
+# ----------------------------
+RCLONE_EXEC = (Path(PROJ_ROOT) / '.github' / 'rclone')
+# ----------------------------
+S3_R2_ACCOUNT_ID = os.getenv('S3_R2_ACCOUNT_ID') or '<mask>'
+S3_R2_ACCESS_KEY = os.getenv('S3_R2_ACCESS_KEY') or '<mask>'
+S3_R2_SECRET_KEY = os.getenv('S3_R2_SECRET_KEY') or '<mask>'
+S3_R2_STORAGE_REGION = os.getenv('S3_R2_STORAGE_REGION') or  'auto'
+S3_R2_STORAGE_BUCKET = os.getenv('S3_R2_STORAGE_BUCKET') or '<mask>'
+# ----------------------------
+CPU_COUNT = os.cpu_count() or 2
+if sys.platform == 'linux':
+    CPU_COUNT = len(os.sched_getaffinity(0))
+# ----------------------------
+NATIVE_PLAT = platform.system().lower()
+if NATIVE_PLAT not in ['linux', 'darwin', 'windows']:
+    raise NotImplementedError(f'unsupported native platform: {NATIVE_PLAT}')
+NATIVE_ARCH = platform.machine().lower()
+if NATIVE_ARCH == 'x86_64':  NATIVE_ARCH = 'amd64'
+if NATIVE_ARCH == 'aarch64': NATIVE_ARCH = 'arm64'
+# ----------------------------
+# >>>> utils functions >>>>
+# ----------------------------
+def print_stderr(str):
+    print(str, file=sys.stderr)
+def _util_func__subprocess(args: Union[str, list[str]],
+    cwd: Union[str, None] = None, env: Union[dict[str, str], None] = None, shell=False,
+    collect_stdout=False, stdout = None,
+) -> str:
+    print_stderr(f'>>>> subprocess cmdline: {args}')
+    proc = sp.run(
+        args=args, cwd=cwd, env=env, check=True, shell=shell,
+        stdout=(sp.PIPE if collect_stdout else stdout), text=(True if collect_stdout else None),
+    )
+    return proc.stdout if collect_stdout else ''
+def _util_load_module(name: str, attrs: Union[list[str], None] = None):
+    module = importlib.import_module(name)
+    for attr in (attrs or []):
+        getattr(module, attr)
+    return module
+def _util_load_pyfile(file: str, attrs: Union[list[str], None] = None):
+    spec = importlib.util.spec_from_file_location('', file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    for attr in (attrs or []):
+        getattr(module, attr)
+    return module
+
+def _util_func__exec_python(args: list[str], cwd: Union[str, None] = None):
+    _util_func__subprocess(args=[(Path(sys.executable)).absolute().as_posix(), *args], cwd=cwd)
+def _util_func__pip_install(packages: list[str]):
+    args = ['-m', 'pip', 'install', '--upgrade']
+    if not ON_GITHUB_CI:
+        args.extend(['--trusted-host', 'repo.huaweicloud.com'])
+        args.extend(['-i', 'https://repo.huaweicloud.com/repository/pypi/simple'])
+    args.extend(['pip', *packages])
+    _util_func__exec_python(args=args)
+def _util_func__dl_3rd_deps(args: list[str]):
+    _util_func__exec_python(args=[
+        (Path(PROJ_ROOT) / 'scripts' / 'get_prebuilt.py').absolute().as_posix(), *args
+    ])
+
+def _util_put_pkg_version_desc(ver: str):
+    (Path(PROJ_ROOT) / 'toolkit.ver') \
+        .write_text(ver, encoding='utf-8')
+def _util_get_pkg_version_desc() -> str:
+    return (Path(PROJ_ROOT) / 'toolkit.ver') \
+        .read_text(encoding='utf-8').strip()
+
+def _util_append_ci_env(f: io.TextIOWrapper, k, v):
+    print_stderr(f'{k}: {v}'); f.write(f'{k}={v}\n')
+def _util_append_env_pkgconf_path(env: dict[str, str], dir: Path):
+    old_pkgconf_path = env.get('PKG_CONFIG_PATH', '')
+    new_pkgconf_path = (dir).absolute().as_posix()
+    env['PKG_CONFIG_PATH'] = new_pkgconf_path + os.pathsep + old_pkgconf_path
+
+def _util_get_cross_toolchain_dir():
+    env = 'CROSS_TOOLCHAIN_ROOT'
+    if dir := os.getenv(env):
+        return dir
+    raise RuntimeError(f'missing required env: `{env}`')
+
+def _util_dopack_zip_with_softlinks(filepath: Path, zipname: Union[str, None] = None):
+    if not zipname:
+        zipname = filepath.name
+
+    _sh = 'bash'
+    _cwd = (filepath.parent).absolute().as_posix()
+    if sys.platform == 'win32':
+        _sh = 'C:/msys64/usr/bin/bash.exe'
+        _cwd = f'$(cygpath -u "{_cwd}")'
+    _cmd = f'zip -ry {zipname}.zip {filepath.name}'
+    _util_func__subprocess(args=[_sh, '-lc', f'cd {_cwd}; {_cmd}'])
+def _util_unpack_zip_with_softlinks(zipfile: Path, extract_dir: str):
+    _sh = 'bash'
+    _cwd = (Path(extract_dir)).absolute().as_posix()
+    if sys.platform == 'win32':
+        _sh = 'C:/msys64/usr/bin/bash.exe'
+        _cwd = f'$(cygpath -u "{_cwd}")'
+    _cmd = f'unzip {zipfile.absolute().as_posix()}'
+    _util_func__subprocess(args=[_sh, '-lc', f'cd {_cwd}; {_cmd}'])
+# ----------------------------
